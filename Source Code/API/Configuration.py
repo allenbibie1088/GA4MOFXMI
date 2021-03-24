9353ec408cba8acaaad8af485bd85e161739bd04
Habibi Husain Arifin <43530087+allenbibie1088@users.noreply.github.com>
2021-03-24 10:05:50 +0700

    Update Configuration.py

--- Source Code/API/Configuration.py
+++ Source Code/API/Configuration.py
@@ -2,7 +2,8 @@
 #Created Date: 10 January 2019
 #Last Updated Date: 23 February 2019
 #Version: 1.0
-#<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
 
 import js2py
 from API.StructureModel import XMI25Element
