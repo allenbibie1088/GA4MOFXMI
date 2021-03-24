ada9725a7f7041099e35916497c41c334e6ff028
Habibi Husain Arifin <43530087+allenbibie1088@users.noreply.github.com>
2021-03-24 10:07:12 +0700

    Update GeneticAlgorithms.py

--- Source Code/API/GeneticAlgorithms.py
+++ Source Code/API/GeneticAlgorithms.py
@@ -1,8 +1,9 @@
 #Author: Habibi Husain Arifin
 #Created Date: 10 January 2019
-#Last Updated Date: 23 February 2019
+#Last Updated Date: 24 March 2021
 #Version: 1.0
-#<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
 
 import random
 import string
